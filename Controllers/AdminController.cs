6fe913dd580aa9a35bea268f565f8fbe3ca06978
Riyad Anabtawi <riyad.faek@gmail.com>
2025-01-28 17:20:56 -0700

    a

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -5,6 +5,7 @@ using PatientManagementSystem.Models;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace PatientManagementSystem.Controllers
 {
