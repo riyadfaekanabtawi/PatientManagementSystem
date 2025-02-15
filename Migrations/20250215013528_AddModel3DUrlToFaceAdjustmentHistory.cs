0df752a63a46bd7a2a7ed7c7e7a6c302646b5ff2
Riyad Anabtawi <riyad.faek@gmail.com>
2025-02-14 18:35:46 -0700

    Changing history

---
+++ Migrations/20250215013528_AddModel3DUrlToFaceAdjustmentHistory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatientManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddModel3DUrlToFaceAdjustmentHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AdjustedImageUrl",
+                table: "FaceAdjustmentHistories",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Model3DUrl",
+                table: "FaceAdjustmentHistories",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Model3DUrl",
+                table: "FaceAdjustmentHistories");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AdjustedImageUrl",
+                table: "FaceAdjustmentHistories",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
